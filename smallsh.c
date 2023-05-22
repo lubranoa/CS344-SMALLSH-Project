9aefd5bb1564a0d601c624d8a616888c247b984f
Alexander Lubrano <lubranoa@oregonstate.edu>
2023-05-21 23:30:33 -0700

    Add some functionality

--- smallsh.c
+++ smallsh.c
@@ -7,6 +7,7 @@
 #include <unistd.h>
 #include <ctype.h>
 #include <string.h>
+#include <sys/wait.h>
 
 #ifndef MAX_WORDS
 #define MAX_WORDS 512
@@ -22,14 +23,19 @@
 
 char *words[MAX_WORDS];
 size_t wordsplit(char const *line);
-char * expand(char const *word);
+char * expand(char const *word, char const *bgpid, char const *pid, char const *status);
 
 int main(int argc, char *argv[])
 {
-  //pid_t smallsh_pid = getpid();
-  //int fg_cmd_status = DEFAULT_EX_STAT;
-  //pid_t bg_pid = EMPTY_STR;
-  pid_t spawn_pid;
+  int length;
+  if ((length = snprintf(0, 0, "%d", getpid())) < 0) err(1, "snprintf");
+  char *smallsh_pid = malloc(sizeof *smallsh_pid * (length + 1));
+  if (sprintf(smallsh_pid, "%d", getpid()) < 0) err(1, "sprintf");
+  
+  char *fg_cmd_status = DEFAULT_EX_STAT;
+  char *bg_pid = EMPTY_STR;
+  pid_t child_pid = -5;
+  int child_status = -5;
 
   FILE *input = stdin;
   char *input_fn = "(stdin)";
@@ -46,49 +52,125 @@ int main(int argc, char *argv[])
   for (;;) {
 prompt:;
     /* TODO: Manage background processes */
-
+    //if (child_pid > 0) {
+    //  if (WIFEXITED(child_status)) fprintf(stderr, "Child provess %d done. Exit status %d.\n", child_pid, WEXITSTATUS(child_status));
+    //  else if (WIFSIGNALED(child_status)) fprintf(stderr, "Child process %d done. Signaled %d.\n", child_pid, WTERMSIG(child_status));
+    //}
     /* TODO: prompt */
     if (input == stdin) {
       char *ps1_str = getenv("PS1");
-      if (fprintf(stderr, "%s", ps1_str) < 0) goto exit;
+      if (ps1_str == NULL) ps1_str = EMPTY_STR;
+      if (fprintf(stderr, "%s", ps1_str) < 0) err(1, "fprintf");
     }
     ssize_t line_len = getline(&line, &n, input);
-    if (line_len < 0) err(1, "%s", input_fn);
-    
+    if (line_len < 0) {
+      if (!feof(input)) {
+        err(1, "%s", input_fn);
+      } else {
+        exit(0);
+      }
+    }
+    memset(words, '\0', sizeof words);
     size_t nwords = wordsplit(line);
     for (size_t i = 0; i < nwords; ++i) {
       //fprintf(stderr, "Word %zu: %s  -->  ", i, words[i]);
-      char *exp_word = expand(words[i]);
+      char *exp_word = expand(words[i], bg_pid, smallsh_pid, fg_cmd_status);
       free(words[i]);
       words[i] = exp_word;
       //fprintf(stderr, "%s\n", words[i]);
     }
+    int bg = 0,
+        rd = 0,
+        wr = 0,
+        ap = 0;
+    char *new_words[MAX_WORDS] = {NULL};
+
+    if (nwords > 1) {
+      for (size_t i = nwords - 1; i > 0; --i) {
+        if (strcmp(words[i], "&") == 0) {
+          bg = 1;
+          words[i] = NULL;
+        }
+        else if (strcmp(words[i], "<") == 0) rd = 1;
+        else if (strcmp(words[i], ">") == 0) wr = 1;
+        else if (strcmp(words[i], ">>") == 0) ap = 1;
+      }
+    }
     if (words[0] == NULL) goto prompt;
 
     if (strncmp(words[0], "exit", 4) == 0) {
-      goto exit;
-    } else if (strncmp(words[0], "cd", 2) == 0) {
-      goto exit;
-    } else {
-      spawn_pid = fork();
+      int exit_num;
+      if (words[2] != NULL) errx(1, "exit: too many arguments");
+      if (words[1] != NULL) {
+        for (int i = 0; i < strlen(words[1]); ++i) {
+          if (words[1][i] < 48 || words[1][i] > 57) errx(1, "exit: argument contained non-digits");
+        }
+        exit_num = atoi(words[1]);
+      } else {
+        exit_num = atoi(fg_cmd_status);
+      }
+      exit(exit_num);
+    } 
+    else if (strncmp(words[0], "cd", 2) == 0) {
+      int dir;
+      char *home_dir = getenv("HOME");
+      if (words[2] != NULL) err(1, "cd: too many arguments");
+      if (words[1] == NULL) {
+        dir = chdir(home_dir);
+      } else {
+        dir = chdir(words[1]);
+      }
+      if (dir == -1) err(1, "chdir");
+    } 
+    else {
+      child_pid = fork();
 
-      switch(spawn_pid) {
+      switch(child_pid) {
         case -1:
           err(1, "fork");
           break;
         case 0:
-          if (execvp(words[0], words) == -1) {
-            err(1, "execvp");
-            break;
-          }
+          execvp(words[0], words);
+          err(1, "execvp");
+          break;
         default:
+          if (bg == 0) {
+            child_pid = waitpid(child_pid, &child_status, 0);
+            if (child_pid == -1) err(1, "waitpid");
+            if ((length = snprintf(0, 0, "%d", child_pid)) < 0) err(1, "snprintf");
+            char *pid = malloc(sizeof *pid * (length + 1));
+            if (sprintf(pid, "%d", child_pid) < 0) err(1, "sprintf");
+            bg_pid = pid;
+            if (WIFEXITED(child_status)) {
+              if ((length = snprintf(0, 0, "%d", WEXITSTATUS(child_status))) < 0) err(1, "snprintf");
+              char *status = malloc(sizeof *status * (length + 1));
+              if (status == NULL) err(1, "malloc");
+              if (sprintf(status, "%d", WEXITSTATUS(child_status)) < 0) err(1, "sprintf");
+              fg_cmd_status = status;
+            } 
+            else {
+              if ((length = snprintf(0, 0, "%d", 128 + WTERMSIG(child_status))) < 0) err(1, "snprintf");
+              char *status = malloc(sizeof *status * (length + 1));
+              if (sprintf(status, "%d", 128 + WTERMSIG(child_status)) < 0) err(1, "sprintf");
+              fg_cmd_status = status;
+            }
+            child_pid = -5;
+            child_status = -5;
+          } 
+          else {
+            if ((length = snprintf(0, 0, "%d", child_pid)) < 0) err(1, "snprintf");
+            char *pid = malloc(sizeof *pid * (length + 1));
+            if (sprintf(pid, "%d", child_pid) < 0) err(1, "sprintf");
+            bg_pid = pid;
+            waitpid(child_pid, &child_status, WNOHANG);
+          }
+          
           break;
       }
     }
   }
-exit:;
-  if (input != stdin) fclose(input);
-  exit(errno);
+  free(smallsh_pid);
+  exit(0);
 }
 
 char *words[MAX_WORDS] = {0};
@@ -202,21 +284,21 @@ build_str(char const *start, char const *end)
  * Returns a newly allocated string that the caller must free
  */
 char *
-expand(char const *word)
+expand(char const *word, char const *bgpid, char const *pid, char const *status)
 {
   char const *pos = word;
   char const *start, *end;
   char c = param_scan(pos, &start, &end);
-  build_str(NULL, NULL);
+  free(build_str(NULL, NULL));
   build_str(pos, start);
   while (c) {
-    if (c == '!') build_str("<BGPID>", NULL);
-    else if (c == '$') build_str("<PID>", NULL);
-    else if (c == '?') build_str("<STATUS>", NULL);
+    if (c == '!') build_str(bgpid, NULL);
+    else if (c == '$') build_str(pid, NULL);
+    else if (c == '?') build_str(status, NULL);
     else if (c == '{') {
-      build_str("<Parameter: ", NULL);
-      build_str(start + 2, end - 1);
-      build_str(">", NULL);
+      char* env_str = getenv(build_str(start + 2, end - 1));
+      free(build_str(NULL, NULL));
+      build_str(env_str, NULL);
     }
     pos = end;
     c = param_scan(pos, &start, &end);
--- test.sh
+++ test.sh
@@ -1,1 +1,1 @@
-SMALLSH_SKIP='2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22' /class/cs344/assignments/02-smallsh/testscript ./smallsh
+SMALLSH_SKIP='1 2 3 4 5 6 10 11 12 13 14 15 16 17 18 19 20 21 22' /class/cs344/assignments/02-smallsh/testscript ./smallsh
